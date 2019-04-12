e9992c4637c7de071b53c61bfbc6829fc3178fc0
Tiernan OToole <tiernan@tiernanotoole.ie>
2019-04-12 11:21:19 +0100

    Add project files.

---
+++ CGNV4-Monitor/Program.cs
@@ -0,0 +1,81 @@
+﻿using Flurl.Http;
+using InfluxDB.Collector;
+using NLog;
+using System;
+using System.Text.RegularExpressions;
+
+namespace CGNV4_Monitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger mLogger = LogManager.GetCurrentClassLogger();
+
+
+            string baseUrl = "http://192.168.100.1";
+
+            var cli = new FlurlClient(baseUrl).EnableCookies().Configure(settings =>
+            {
+                settings.BeforeCall = call => mLogger.Debug($"Calling Netgear Url {call.Request.RequestUri}");
+                settings.OnError = call => mLogger.Error($"Error calling Netgear: {call.RequestBody} -  {call.Exception}");
+                settings.AfterCall = call =>
+                    mLogger.Debug($"called Netgear with {call.HttpStatus} in {call.Duration}");
+            });
+
+            var indexPage = cli.Request("/").GetStringAsync().GetAwaiter().GetResult();
+
+            string preSessionKey = cli.Cookies["preSession"].Value;
+
+            var login = cli.Request("goform/login").PostUrlEncodedAsync(new { usr = "admin", pwd = "admin" , preSession = preSessionKey, forcelogoff = 1}).ReceiveString().GetAwaiter().GetResult();
+
+            var user = cli.Request("data/getUser.asp").GetStringAsync().GetAwaiter().GetResult();
+
+            var sysInfo = cli.Request("data/getSysInfo.asp").GetJsonAsync<SystemInfo[]>().GetAwaiter().GetResult();
+
+            var cmInfo = cli.Request("data/getCMInit.asp").GetJsonAsync<CMInfo[]>().GetAwaiter().GetResult();
+
+            var upstreamInfo = cli.Request("data/usinfo.asp").GetJsonAsync<StreamInfo[]>().GetAwaiter().GetResult();
+
+            var downstreamInfo = cli.Request("data/dsinfo.asp").GetJsonAsync<StreamInfo[]>().GetAwaiter().GetResult();
+
+            var cmDocsisWan = cli.Request("data/getCmDocsisWan.asp").GetJsonAsync<DocsisWanInfo[]>().GetAwaiter().GetResult();
+
+            //urls i found:
+
+            //http://192.168.100.1/data/getSysInfo.asp?_=1555060334993
+            //http://192.168.100.1/data/dsinfo.asp?_=1555059523324
+            //http://192.168.100.1/data/getCmDocsisWan.asp?_=1555059523327
+            //http://192.168.100.1/data/usinfo.asp?_=1555059523329
+            //http://192.168.100.1/data/getTuneFreq.asp?_=1555059523332
+            //http://192.168.100.1/data/status_log.asp?_=1555059607669
+            //http://192.168.100.1/data/wireless_basic.asp?_=1555059624496
+            //http://192.168.100.1/data/wireless_ssid.asp?_=1555059624497
+            //http://192.168.100.1/data/wireless_wifi4all.asp?_=1555059624501
+            //http://192.168.100.1/data/mta_status.asp?_=1555059659383
+
+
+
+            //mLogger.Debug("Calling out to telegraf");
+            //Metrics.Collector = new CollectorConfiguration()
+            //    .Tag.With("host", Environment.GetEnvironmentVariable("COMPUTERNAME"))
+            //    .Batch.AtInterval(TimeSpan.FromSeconds(30))
+            //    .WriteTo.InfluxDB("http://192.168.1.119:8086", "telegraf")
+            //    .CreateCollector();
+
+
+            //while (true)
+            //{
+            //    try
+            //    {
+            //        var model = cli.Request("/api/model.json").SetQueryParam("internalapi", "1").GetJsonAsync<Model>().GetAwaiter().GetResult();
+
+            //    }
+            //    catch(Exception ex)
+            //    {
+
+            //    }
+            //}
+        }
+    }
+}
---
+++ CGNV4-Monitor/systeminfo.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CGNV4_Monitor
+{
+    public class SystemInfo
+    {
+        public string hwVersion { get; set; }
+        public string swVersion { get; set; }
+        public string serialNumber { get; set; }
+        public string rfMac { get; set; }
+        public string wanIp { get; set; }
+        public string aftrName { get; set; }
+        public string aftrAddr { get; set; }
+        public string delegatedPrefix { get; set; }
+        public string lanIPv6Addr { get; set; }
+        public string systemUptime { get; set; }
+        public string systemTime { get; set; }
+        public string timezone { get; set; }
+        public string WRecPkt { get; set; }
+        public string WSendPkt { get; set; }
+        public string lanIp { get; set; }
+        public string LRecPkt { get; set; }
+        public string LSendPkt { get; set; }
+    }
+
+    public class CMInfo
+    {
+        public string hwInit { get; set; }
+        public string findDownstream { get; set; }
+        public string ranging { get; set; }
+        public string dhcp { get; set; }
+        public string timeOfday { get; set; }
+        public string downloadCfg { get; set; }
+        public string registration { get; set; }
+        public string eaeStatus { get; set; }
+        public string bpiStatus { get; set; }
+        public string networkAccess { get; set; }
+        public string trafficStatus { get; set; }
+    }
+
+
+
+
+    public class DocsisWanInfo
+    {
+        public string Configname { get; set; }
+        public string NetworkAccess { get; set; }
+        public string CmIpAddress { get; set; }
+        public string CmNetMask { get; set; }
+        public string CmGateway { get; set; }
+        public string CmIpLeaseDuration { get; set; }
+    }
+
+
+
+    public class StreamInfo
+    {
+        public string portId { get; set; }
+        public string frequency { get; set; }
+        public string bandwidth { get; set; }
+        public string scdmaMode { get; set; }
+        public string signalStrength { get; set; }
+        public string channelId { get; set; }
+    }
+
+
+    
+    public class EventInfo
+    {
+        public int index { get; set; }
+        public string time { get; set; }
+        public string type { get; set; }
+        public string priority { get; set; }
+        public string _event { get; set; }
+    }
+
+
+
+
+}
